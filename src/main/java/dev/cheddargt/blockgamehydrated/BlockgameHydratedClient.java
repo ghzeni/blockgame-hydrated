aaf85069a2d76f1cfdf533c53afd73602e608023
cheddargt <gus_ghz@live.com>
2024-09-20 09:03:34 -0300

    first commit

---
+++ src/main/java/dev/cheddargt/blockgamehydrated/BlockgameHydratedClient.java
@@ -0,0 +1,10 @@
+package dev.cheddargt.blockgamehydrated;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockgameHydratedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
